b0c22b3e5b322cc56a6130c97606f795b5792a87
Ekrar Efaz <ekrar@Ekrars-MacBook-Air.local>
2024-03-12 11:57:08 +1100

    grid draw complete

--- main.py
+++ main.py
@@ -18,53 +18,20 @@ def grid_init(grid_size, agent_position, goal_positions, wall_positions):
     for wall in wall_positions:
         grid.add_wall(*wall)
 
+    """ Set pointers for neighboring cells """
+    grid.set_neighbors()
+
     return grid
 
-""" set pointers for neighb
-oring cells by direction"""
-def set_neighbors(grid: Grid, grid_size):
-    x_cell_count, y_cell_count = grid_size
 
-    """ iterate over each cell in the grid by grid[column][row] """
-    column_index = 0
-    while (column_index < x_cell_count):
-        row_index = 0
-
-        while (row_index < y_cell_count):
-
-            """ set pointers for north """
-            if row_index > 0:
-                grid.grid[column_index][row_index].north = grid.grid[column_index][row_index - 1]
-            else:
-                grid.grid[column_index][row_index].north = None
-            
-            """ set pointers for south """
-            if row_index < y_cell_count - 1:
-                grid.grid[column_index][row_index].south = grid.grid[column_index][row_index + 1]
-            else:
-                grid.grid[column_index][row_index].south = None
-
-            """ set pointers for west """
-            if column_index > 0:
-                grid.grid[column_index][row_index].west = grid.grid[column_index - 1][row_index]
-            else:
-                grid.grid[column_index][row_index].west = None
-
-            """ set pointers for east """
-            if column_index > 0:
-                grid.grid[column_index][row_index].east = grid.grid[column_index + 1][row_index]
-            else:
-                grid.grid[column_index][row_index].east = None
 
 def main():
     grid_size, agent_position, goal_positions, wall_positions = parser()
-    
     grid = grid_init(grid_size, agent_position, goal_positions, wall_positions)
+
     #DEBUG
     print("grid created")
+    grid.print_grid()
 
-    set_neighbors(grid, grid_size)
-    #DEBUG
-    print("neighbors created")
 
 main()
--- models/grid.py
+++ models/grid.py
@@ -5,28 +5,71 @@ from models.cell import Cell
 from models.wall import Wall
 
 class Grid:
-    def __init__(self, width, height):
+    def __init__(self, height, width):
         self.width = width
         self.height = height
         self.grid = []
         self.build_grid()
 
+    """ iterate over each row and create columns for each row """
     def build_grid(self):
-        # Initialize the grid with Cell instances
         self.grid = [[Cell(x, y) for x in range(self.width)] for y in range(self.height)]
 
+    """ create and set agent position within grid """
     def set_agent(self, x, y):
-        # Ensure the cell exists
         if 0 <= y < self.height and 0 <= x < self.width:
             self.grid[y][x] = Agent(x, y)
 
+    """ create goal cells within grid """
     def add_goal(self, x, y):
-        # Ensure the cell exists
         if 0 <= y < self.height and 0 <= x < self.width:
             self.grid[y][x] = Goal(x, y)
 
+    """ create walls given the wall specs within grid bounds """
     def add_wall(self, x, y, width, height):
-        # Ensure the wall fits within the grid bounds
         for i in range(x, min(x + width, self.width)):
             for j in range(y, min(y + height, self.height)):
                 self.grid[j][i] = Wall(i, j, width, height)
+
+    """ set pointers for neighboring cells by direction"""
+    def set_neighbors(self):
+        for row_index in range(self.height):  
+            for column_index in range(self.width):  
+
+                # Set pointers for north
+                if row_index > 0:
+                    self.grid[row_index][column_index].north = self.grid[row_index - 1][column_index]
+                else:
+                    self.grid[row_index][column_index].north = None
+
+                # Set pointers for south
+                if row_index < self.height - 1:
+                    self.grid[row_index][column_index].south = self.grid[row_index + 1][column_index]
+                else:
+                    self.grid[row_index][column_index].south = None
+
+                # Set pointers for west
+                if column_index > 0:
+                    self.grid[row_index][column_index].west = self.grid[row_index][column_index - 1]
+                else:
+                    self.grid[row_index][column_index].west = None
+
+                # Set pointers for east
+                if column_index < self.width - 1:
+                    self.grid[row_index][column_index].east = self.grid[row_index][column_index + 1]
+                else:
+                    self.grid[row_index][column_index].east = None
+
+    def print_grid(self):
+        for row_index in range(self.height):
+            print("\n")
+            for column_index in range(self.width):
+                if isinstance(self.grid[row_index][column_index], Wall):
+                    print(" W ", end="")
+                elif isinstance(self.grid[row_index][column_index], Agent):
+                    print(" A ", end="")
+                elif isinstance(self.grid[row_index][column_index], Goal):
+                    print(" G ", end="")
+                elif isinstance(self.grid[row_index][column_index], Cell):
+                    print(" . ", end="")
+  
