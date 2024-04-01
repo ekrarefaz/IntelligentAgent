9c97d14861a3781af9f0fc3de281148bd354d836
Ekrar Efaz <ekrar@Ekrars-MacBook-Air.local>
2024-04-01 23:27:07 +1100

    Add Plot Performance Visuals

--- main.py
+++ main.py
@@ -7,27 +7,7 @@ from searches.dfs import DFS
 from searches.gbfs import GBFS
 from searches.a_star import AS
 from searches.iddfs import IDDFS
-
-def grid_init(grid_size, agent_position, goal_positions, wall_positions):
-
-    """ Initialize the grid with Data from data parser """
-    grid = Grid(*grid_size)
-
-    """ Initialize the agent in the grid """
-    grid.set_agent(*agent_position)
-
-    """ Initialize the goals in the grid """
-    for goal in goal_positions:
-        grid.add_goal(*goal)
-
-    """ Initialize the walls in the grid """
-    for wall in wall_positions:
-        grid.add_wall(*wall)
-
-    """ Set pointers for neighboring cells """
-    grid.set_neighbors()
-    return grid
-
+from utils.grid_init import grid_init
 
 def dfs(grid: Grid):
     search_agent = DFS(grid)
