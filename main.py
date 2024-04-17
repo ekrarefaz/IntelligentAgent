2c03918e64a3b85934a52efd1c784c0c56a0a5ae
Ekrar Efaz <ekrar@Ekrars-MacBook-Air.local>
2024-04-17 21:23:45 +1000

    Refactor main() using argparse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from utils.data_parser import gridparser
 from models.cell import Cell
 from models.grid import Grid
@@ -10,6 +11,9 @@ from searches.iddfs import IDDFS
 from searches.idas import IDAS
 from utils.grid_init import grid_init
 
+"""
+Search Algorithms
+"""
 def dfs(grid: Grid):
     search_agent = DFS(grid)
     search_agent.search()
@@ -35,24 +39,38 @@ def idas(grid: Grid):
     search_agent.search()
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python3 [FILENAME.py] [METHOD]")
-        sys.exit(1)
+    """
+    Parse the command line arguments
+    """
+    parser = argparse.ArgumentParser(description="Run Search Algorithms on Grids")
+    parser.add_argument('filename', type=str, help="Path to the grid configuration file.")
+    parser.add_argument('method', type=str, help="Search method to use.")
+    args = parser.parse_args()
     
-    method = sys.argv[2].lower() 
-    grid_size, agent_position, goal_positions, wall_positions = gridparser(sys.argv[1])
+    gridfile = args.filename
+    method = args.method.upper()
 
-    grid = grid_init(grid_size, agent_position, goal_positions, wall_positions)
-
+    """
+    Search methods dictionary
+    """
     search_methods = {
-        'bfs': bfs,
-        'dfs': dfs,
-        'gbfs': gbfs,
-        'astar': astar,
-        'iddfs': iddfs,
-        'idas' : idas
+        'BFS': bfs,
+        'DFS': dfs,
+        'GBFS': gbfs,
+        'AS': astar,
+        'IDDFS': iddfs,
+        'IDAS' : idas
     }
 
+    """
+    Parse the grid file and initialize the grid
+    """
+    grid_size, agent_position, goal_positions, wall_positions = gridparser(gridfile)
+    grid = grid_init(grid_size, agent_position, goal_positions, wall_positions)
+
+    """
+    Execute the search method
+    """
     search_function = search_methods.get(method)
     if search_function:
         print(f"Executing {method.upper()} search...")
